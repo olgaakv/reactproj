cd4664246ca731c4fa809b6524a3bcbda6bf10f4
olga chistyakova <57174200+olgaakv@users.noreply.github.com>
2019-11-08 11:31:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import {
   View,
   Text,
   StatusBar,
+  AppState,
+  Alert
 } from 'react-native';
 
 import {
@@ -23,6 +25,8 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import Push from 'appcenter-push';
 
 const App: () => React$Node = () => {
   return (
@@ -112,3 +116,32 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+Push.setListener({
+  onPushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+
+    if (message === null) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = 'Android background';
+      message = '<empty>';
+    }
+
+    // Custom name/value pairs set in the App Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
